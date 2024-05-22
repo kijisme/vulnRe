610f66d830bb1f370c9fd48e9b9c38e8311123d8
Mao wenxin <112704604+kijisme@users.noreply.github.com>
2024-05-22 06:19:32 +0000

    2024-5-17 添加修改

--- Source.c
+++ Source.c
@@ -21,7 +21,7 @@ uint8_t unused1[64];
 uint8_t array1[160] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 uint8_t unused2[64];
 uint8_t array2[256 * 512];
-
+// why unused1 ? unused2
 char* secret = "Magic Words";
 
 uint8_t temp = 0; 
@@ -139,7 +139,7 @@ int main(int argc, const char* * argv)
 				   score[1]);
 		printf("\n");
 	}
-	
+
 #ifdef _MSC_VER
 	printf("Press ENTER to exit\n");
 	getchar();	/* Pause Windows console */
